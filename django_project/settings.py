f577bd163d96115f1269653a73d63ccf52ef5823
dmurawski <dmurawski018@gmail.com>
2024-11-12 16:26:51 +0100

    fix allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,7 +19,11 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "bookstore-production-09be.up.railway.app",
+]
 
 
 # Application definition

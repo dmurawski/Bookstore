a00590c39d51e0b5c2f838a8a4f4def2e1013bb5
dmurawski <dmurawski018@gmail.com>
2024-11-12 16:09:14 +0100

    fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -86,20 +86,6 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# DATABASES = {
-#     "default": env.dj_db_url(
-#         "DATABASE_URL",
-#         default="postgres://postgres@db/postgres",
-#     )
-# }
-
 # Railway
 DATABASES = {
     "default": {

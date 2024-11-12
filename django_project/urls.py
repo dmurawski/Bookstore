cd02abdd52c0d3b7602323da02dbc4352ccadfe0
dmurawski <dmurawski018@gmail.com>
2024-11-12 15:20:01 +0100

    change admin url

--- django_project/urls.py
+++ django_project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Django admin
-    path("admin/", admin.site.urls),
+    path("admin-site/", admin.site.urls),
     # User management
     path("accounts/", include("allauth.urls")),
     # Local apps
